bd099e4306d0825186979cb49bae677cd61d854f
Brian Chen <brian.chen@tabcorp.com.au>
2021-12-10 05:51:33 +1100

    upgrade commit hooks

--- src/bin/checkDocs.js
+++ src/bin/checkDocs.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 // @flow
-
 import fs from 'fs';
 import path from 'path';
+
 import {
   getRules,
   isFile,
@@ -23,7 +23,7 @@ const getDocIndexRules = () => {
   const content = fs.readFileSync(path.resolve(__dirname, '../../.README/README.md'), 'utf-8');
 
   const rules = content.split('\n').map((line) => {
-    const match = /^{"gitdown": "include", "file": "([^"]+)"}$/u.exec(line);
+    const match = /^\{"gitdown": "include", "file": "([^"]+)"\}$/u.exec(line);
 
     if (match === null) {
       return null;
@@ -31,9 +31,7 @@ const getDocIndexRules = () => {
 
     return match[1].replace('./rules/', '').replace('.md', '');
   })
-    .filter((rule) => {
-      return rule !== null;
-    });
+    .filter((rule) => rule !== null);
 
   if (rules.length === 0) {
     throw new Error('Docs checker is broken - it could not extract rules from docs index file.');
@@ -57,7 +55,8 @@ const hasCorrectAssertions = (docPath, name) => {
 /**
  * Performed checks:
  *  - file `/.README/rules/<rule>.md` exists
- *  - file `/.README/rules/<rule>.md` contains correct assertions placeholder (`<!-- assertions ... -->`)
+ *  - file `/.README/rules/<rule>.md` contains correct assertions placeholder
+ *    (`<!-- assertions ... -->`)
  *  - rule is included in gitdown directive in `/.README/README.md`
  *  - rules in `/.README/README.md` are alphabetically sorted
  */
@@ -65,16 +64,14 @@ const checkDocs = (rulesNames) => {
   const docIndexRules = getDocIndexRules();
 
   const sorted = windows(docIndexRules, 2)
-    .every((chunk) => {
-      return chunk[0] < chunk[1];
-    });
+    .every((chunk) => chunk[0] < chunk[1]);
 
   if (!sorted) {
     throw new Error('Rules are not alphabetically sorted in `.README/README.md` file.');
   }
 
   const invalid = rulesNames.filter((names) => {
-    const docPath = path.resolve(__dirname, '../../.README/rules', names[1] + '.md');
+    const docPath = path.resolve(__dirname, '../../.README/rules', `${names[1]}.md`);
     const docExists = isFile(docPath);
     const inIndex = docIndexRules.includes(names[1]);
     const hasAssertions = docExists ? hasCorrectAssertions(docPath, names[0]) : false;
@@ -84,14 +81,12 @@ const checkDocs = (rulesNames) => {
 
   if (invalid.length > 0) {
     const invalidList = invalid
-      .map((names) => {
-        return names[0];
-      }).join(', ');
+      .map((names) => names[0]).join(', ');
 
     throw new Error(
-      'Docs checker encountered an error in: ' + invalidList + '. ' +
-      'Make sure that for every rule you created documentation file with assertions placeholder in camelCase ' +
-      'and included the file path in `.README/README.md` file.',
+      `Docs checker encountered an error in: ${invalidList}. `
+      + 'Make sure that for every rule you created documentation file with assertions placeholder in camelCase '
+      + 'and included the file path in `.README/README.md` file.',
     );
   }
 };
