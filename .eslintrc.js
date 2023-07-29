845bdd4496f3daf6ebdbb4bbec1dfc16ae6bc2e2
Brian Chen <bzchen93@gmail.com>
2023-07-29 12:12:58 +1000

    run node 18 also

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     'global-require': 0,
     'import/no-dynamic-require': 0,
     // Overriding from https://github.com/airbnb/javascript/blob/fd77bbebb77362ddecfef7aba3bf6abf7bdd81f2/packages/eslint-config-airbnb-base/rules/style.js#L206C16-L212C8
-    'max-len': ['error', 120, 2, {
+    'max-len': ['error', 200, 2, {
       ignoreUrls: true,
       ignoreComments: false,
       ignoreRegExpLiterals: true,
